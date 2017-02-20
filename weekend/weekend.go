b4f6d8802de1751231666692bcee7072da6ff100
Jacek J. Łakis <jacek.lakis@intel.com>
2017-02-20 13:38:25 +0100

    Implement CollectMetrics() with hardcoded values

--- weekend/weekend.go
+++ weekend/weekend.go
@@ -20,10 +20,12 @@ limitations under the License.
 package weekend
 
 import (
-	"fmt"
+	"log"
+	"time"
 
 	"github.com/intelsdi-x/snap/control/plugin"
 	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/core"
 )
 
 const (
@@ -41,21 +43,58 @@ type Weekend struct {
 
 // CollectMetrics collects metrics for testing
 func (f *Weekend) CollectMetrics(mts []plugin.MetricType) ([]plugin.MetricType, error) {
-	return []plugin.MetricType{}, fmt.Errorf("Not implemented")
+	for _, p := range mts {
+		log.Printf("collecting %+v\n", p)
+	}
+
+	metrics := []plugin.MetricType{}
+	for i := range mts {
+		switch mts[i].Namespace()[2].Value {
+		case "daystillfriday":
+			mts[i].Data_ = 1
+		case "isitweekend":
+			mts[i].Data_ = "false"
+		}
+
+		mts[i].Timestamp_ = time.Now()
+		metrics = append(metrics, mts[i])
+	}
+
+	return metrics, nil
 }
 
 // GetMetricTypes returns metric types for testing
 func (f *Weekend) GetMetricTypes(cfg plugin.ConfigType) ([]plugin.MetricType, error) {
-	return []plugin.MetricType{}, fmt.Errorf("Not implemented")
+	mts := []plugin.MetricType{}
+
+	mts = append(mts, plugin.MetricType{
+		Namespace_:   core.NewNamespace("jjlakis", "weekend", "daystillfriday"),
+		Description_: "Days till friday",
+		Unit_:        "Days",
+	})
+	mts = append(mts, plugin.MetricType{
+		Namespace_:   core.NewNamespace("jjlakis", "weekend", "isitweekend"),
+		Description_: "Is it weekend (Friday, Saturday, Sunday)?",
+		Unit_:        "True/false",
+	})
+	return mts, nil
 
 }
 
 // GetConfigPolicy returns a ConfigPolicy for testing
 func (f *Weekend) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
-	return nil, fmt.Errorf("Not implemented")
+	return cpolicy.New(), nil
 }
 
 // Meta returns meta data for testing
 func Meta() *plugin.PluginMeta {
-	return nil
+	return plugin.NewPluginMeta(
+		Name,
+		Version,
+		Type,
+		[]string{plugin.SnapGOBContentType},
+		[]string{plugin.SnapGOBContentType},
+		plugin.CacheTTL(100*time.Millisecond),
+		plugin.RoutingStrategy(plugin.StickyRouting),
+	)
 }
